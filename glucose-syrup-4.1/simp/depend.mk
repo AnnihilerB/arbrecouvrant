0eca21d48b10178482136b961d3817e0237bacc9
Ali <acherifi@etu.u-bordeaux.fr>
2017-10-30 15:23:27 +0100

    Ajout de glucose compilé

---
+++ glucose-syrup-4.1/simp/depend.mk
@@ -0,0 +1,42 @@
+/autofs/netapp/travail/acherifi/arbrecouvrant/glucose-syrup-4.1/simp/SimpSolver.o /autofs/netapp/travail/acherifi/arbrecouvrant/glucose-syrup-4.1/simp/SimpSolver.or /autofs/netapp/travail/acherifi/arbrecouvrant/glucose-syrup-4.1/simp/SimpSolver.od /autofs/netapp/travail/acherifi/arbrecouvrant/glucose-syrup-4.1/simp/SimpSolver.op: \
+ /autofs/netapp/travail/acherifi/arbrecouvrant/glucose-syrup-4.1/simp/SimpSolver.cc \
+ /autofs/netapp/travail/acherifi/arbrecouvrant/glucose-syrup-4.1/simp/../mtl/Sort.h \
+ /autofs/netapp/travail/acherifi/arbrecouvrant/glucose-syrup-4.1/simp/../mtl/Vec.h \
+ /autofs/netapp/travail/acherifi/arbrecouvrant/glucose-syrup-4.1/simp/../mtl/IntTypes.h \
+ /autofs/netapp/travail/acherifi/arbrecouvrant/glucose-syrup-4.1/simp/../mtl/XAlloc.h \
+ /autofs/netapp/travail/acherifi/arbrecouvrant/glucose-syrup-4.1/simp/../simp/SimpSolver.h \
+ /autofs/netapp/travail/acherifi/arbrecouvrant/glucose-syrup-4.1/simp/../mtl/Queue.h \
+ /autofs/netapp/travail/acherifi/arbrecouvrant/glucose-syrup-4.1/simp/../core/Solver.h \
+ /autofs/netapp/travail/acherifi/arbrecouvrant/glucose-syrup-4.1/simp/../mtl/Heap.h \
+ /autofs/netapp/travail/acherifi/arbrecouvrant/glucose-syrup-4.1/simp/../mtl/Alg.h \
+ /autofs/netapp/travail/acherifi/arbrecouvrant/glucose-syrup-4.1/simp/../utils/Options.h \
+ /autofs/netapp/travail/acherifi/arbrecouvrant/glucose-syrup-4.1/simp/../utils/ParseUtils.h \
+ /autofs/netapp/travail/acherifi/arbrecouvrant/glucose-syrup-4.1/simp/../core/SolverTypes.h \
+ /autofs/netapp/travail/acherifi/arbrecouvrant/glucose-syrup-4.1/simp/../mtl/Map.h \
+ /autofs/netapp/travail/acherifi/arbrecouvrant/glucose-syrup-4.1/simp/../mtl/Alloc.h \
+ /autofs/netapp/travail/acherifi/arbrecouvrant/glucose-syrup-4.1/simp/../core/BoundedQueue.h \
+ /autofs/netapp/travail/acherifi/arbrecouvrant/glucose-syrup-4.1/simp/../core/Constants.h \
+ /autofs/netapp/travail/acherifi/arbrecouvrant/glucose-syrup-4.1/simp/../mtl/Clone.h \
+ /autofs/netapp/travail/acherifi/arbrecouvrant/glucose-syrup-4.1/simp/../core/SolverStats.h \
+ /autofs/netapp/travail/acherifi/arbrecouvrant/glucose-syrup-4.1/simp/../utils/System.h
+/autofs/netapp/travail/acherifi/arbrecouvrant/glucose-syrup-4.1/simp/Main.o /autofs/netapp/travail/acherifi/arbrecouvrant/glucose-syrup-4.1/simp/Main.or /autofs/netapp/travail/acherifi/arbrecouvrant/glucose-syrup-4.1/simp/Main.od /autofs/netapp/travail/acherifi/arbrecouvrant/glucose-syrup-4.1/simp/Main.op: \
+ /autofs/netapp/travail/acherifi/arbrecouvrant/glucose-syrup-4.1/simp/Main.cc \
+ /autofs/netapp/travail/acherifi/arbrecouvrant/glucose-syrup-4.1/simp/../utils/System.h \
+ /autofs/netapp/travail/acherifi/arbrecouvrant/glucose-syrup-4.1/simp/../mtl/IntTypes.h \
+ /autofs/netapp/travail/acherifi/arbrecouvrant/glucose-syrup-4.1/simp/../utils/ParseUtils.h \
+ /autofs/netapp/travail/acherifi/arbrecouvrant/glucose-syrup-4.1/simp/../utils/Options.h \
+ /autofs/netapp/travail/acherifi/arbrecouvrant/glucose-syrup-4.1/simp/../mtl/Vec.h \
+ /autofs/netapp/travail/acherifi/arbrecouvrant/glucose-syrup-4.1/simp/../mtl/XAlloc.h \
+ /autofs/netapp/travail/acherifi/arbrecouvrant/glucose-syrup-4.1/simp/../core/Dimacs.h \
+ /autofs/netapp/travail/acherifi/arbrecouvrant/glucose-syrup-4.1/simp/../core/SolverTypes.h \
+ /autofs/netapp/travail/acherifi/arbrecouvrant/glucose-syrup-4.1/simp/../mtl/Alg.h \
+ /autofs/netapp/travail/acherifi/arbrecouvrant/glucose-syrup-4.1/simp/../mtl/Map.h \
+ /autofs/netapp/travail/acherifi/arbrecouvrant/glucose-syrup-4.1/simp/../mtl/Alloc.h \
+ /autofs/netapp/travail/acherifi/arbrecouvrant/glucose-syrup-4.1/simp/../simp/SimpSolver.h \
+ /autofs/netapp/travail/acherifi/arbrecouvrant/glucose-syrup-4.1/simp/../mtl/Queue.h \
+ /autofs/netapp/travail/acherifi/arbrecouvrant/glucose-syrup-4.1/simp/../core/Solver.h \
+ /autofs/netapp/travail/acherifi/arbrecouvrant/glucose-syrup-4.1/simp/../mtl/Heap.h \
+ /autofs/netapp/travail/acherifi/arbrecouvrant/glucose-syrup-4.1/simp/../core/BoundedQueue.h \
+ /autofs/netapp/travail/acherifi/arbrecouvrant/glucose-syrup-4.1/simp/../core/Constants.h \
+ /autofs/netapp/travail/acherifi/arbrecouvrant/glucose-syrup-4.1/simp/../mtl/Clone.h \
+ /autofs/netapp/travail/acherifi/arbrecouvrant/glucose-syrup-4.1/simp/../core/SolverStats.h
