b137191be134a6cd71bb43ff1d50f2719522af8b
Pierre Lorson <plorson@emi.u-bordeaux1.fr>
2017-10-30 16:42:16 +0100

    Algo clauses 2/3/4 codé, reste clause 1

--- main.c
+++ main.c
@@ -3,6 +3,28 @@
 #include <unistd.h>
 #include <fcntl.h>
 #include <string.h>
+#include "graphes/all.h"
+
+int orderG(){
+return 3;}
+
+int sizeG(){
+return 2;
+}
+
+
+int graph[3][3] = {
+  // 0  1  2  3  4  5  6  7  8  9 10 11
+    {0, 1, 1}, //0
+    {1, 0, 0}, //1
+    {1, 0, 0}
+};
+
+int are_adjacent(int u, int v){
+      return graph[u][v];
+}
+
+
 
 void clause2(int filedesc, int n){
     for(int i = 1; i<=n; i++){
@@ -15,13 +37,54 @@ void clause2(int filedesc, int n){
                 write(filedesc,"0",1);
                 write(filedesc," ",1);
                 write(filedesc,"-",1);
-                snprintf(buf,2, "%d", j);                
+                snprintf(buf,2, "%d", j);
                 write(filedesc,buf,1);
                 write(filedesc,"0",1);
+                write(filedesc," ",1);
+                write(filedesc,"0",1);
                 write(filedesc,"\n",1);
+
             }
         }
     }
+}
+
+void clause4(int filedesc, int n, int k){
+  char buf[2];
+  for(int i = 1; i<=n; i++){
+      for(int ktmp = 1; ktmp<=k; ktmp++){
+        snprintf(buf, 2, "%d", i);
+        write(filedesc,"-",1);
+        write(filedesc,buf,1);
+        snprintf(buf, 2, "%d", ktmp);
+        write(filedesc,buf,1);
+        write(filedesc," ",1);
+        for(int j = 1; j<=n; j++){
+          if(are_adjacent(i-1,j-1)){
+            snprintf(buf, 2, "%d", j);
+            write(filedesc,buf,1);
+            snprintf(buf, 2, "%d", ktmp-1);
+            write(filedesc,buf,1);
+            write(filedesc," ",1);
+          }
+        }
+        write(filedesc,"0",1);
+        write(filedesc,"\n",1);
+        }
+      }
+    }
+
+void clause3(int filedesc, int n, int k){
+	char buf[2];
+	for(int i = 1; i<=n; i++){
+		snprintf(buf, 2, "%d", i);
+        write(filedesc,buf,1);
+        snprintf(buf, 2, "%d", k);
+        write(filedesc,buf,1);
+        write(filedesc," ",1);
+	}
+	write(filedesc,"0",1);
+	write(filedesc,"\n",1);
 }
 
 
@@ -29,7 +92,10 @@ int main(){
     int filedesc = open("form.cnf", O_WRONLY | O_CREAT,0666);
 
     int n = 3;
+    int k = 2;
     clause2(filedesc, n);
+    clause3(filedesc, n, k);
+    clause4(filedesc, n, k);
+
+
 }
-
-
