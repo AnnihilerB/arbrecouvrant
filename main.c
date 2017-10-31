d78b9069f5516a6a9b09d94e589535d502332e15
Ali <cherifiali@outlook.fr>
2017-10-31 22:43:21 +0100

    Ajout de l'algo pour la clause 4

--- main.c
+++ main.c
@@ -6,41 +6,83 @@
 #include "graphes/all.h"
 
 int orderG(){
-return 3;}
+return 4;}
 
 int sizeG(){
 return 2;
 }
 
 
-int graph[3][3] = {
-  // 0  1  2  3  4  5  6  7  8  9 10 11
-    {0, 1, 1}, //0
-    {1, 0, 0}, //1
-    {1, 0, 0}
+int graph[4][4] = {
+  // 0  1  2
+    {0, 1, 1, 1}, //0
+    {1, 0, 1, 0}, //1
+    {1, 1, 0, 0},  //2
+    {1, 0, 0, 0}
 };
 
 int are_adjacent(int u, int v){
       return graph[u][v];
 }
 
+void clause1(int filedesc, int n, int k){
+    for (int i=1; i<=n; i++){
+        int h_suppose = 0;
+        for (int p = 0; p<=k; p++){
+            for (int h_impossible = 0; h_impossible <= k; h_impossible++){
+                char buf[2];
+                if (h_suppose != h_impossible){
+                    //Sommet de référence
+                    snprintf(buf, 2, "%d", i);
+                    write(filedesc,"-",1);
+                    write(filedesc,buf,1);
+                    //Hauteur supposée vraie
+                    snprintf(buf, 2, "%d", h_suppose);
+                    write(filedesc,buf,1);
+                    write(filedesc," ",1);
+                    //Sommet de référence
+                    snprintf(buf, 2, "%d", i);
+                    write(filedesc,"-",1);
+                    write(filedesc,buf,1);
+                    //Hauteur impossible pour ce sommet
+                    snprintf(buf, 2, "%d", h_impossible);
+                    write(filedesc,buf,1);
+                    write(filedesc," ",1);
+                    //Fin de ligne
+                    write(filedesc,"0",1);
+                    write(filedesc,"\n",1);
+                }
+            }
+            h_suppose++;
+        }
+
+    }
+}
 
 
+/* Un unique sommet de hauteur 0 */
 void clause2(int filedesc, int n){
-    for(int i = 1; i<=n; i++){
+    // Parcours des différents sommets.
+    for(int i = 1; i<=n; i++){ 
         for(int j = 1; j<=n; j++){
             char buf[2];
+            //Permet de ne pas comparer un sommet a lui même
             if(i!=j){
+                //Sommet de référence
                 snprintf(buf, 2, "%d", i);
                 write(filedesc,"-",1);
                 write(filedesc,buf,1);
+                //Hauteur 0
                 write(filedesc,"0",1);
                 write(filedesc," ",1);
+                //Sommet de référence
                 write(filedesc,"-",1);
                 snprintf(buf,2, "%d", j);
                 write(filedesc,buf,1);
+                //Hauteur 0
                 write(filedesc,"0",1);
                 write(filedesc," ",1);
+                //Fin de ligne
                 write(filedesc,"0",1);
                 write(filedesc,"\n",1);
 
@@ -49,53 +91,70 @@ void clause2(int filedesc, int n){
     }
 }
 
+/* Si un sommet est de hauteur >0, alors il dispose d'un parent si l'arrête existe dans le graphe. */
 void clause4(int filedesc, int n, int k){
   char buf[2];
+  //Sommet de référence
   for(int i = 1; i<=n; i++){
+      //Itération sur K
       for(int ktmp = 1; ktmp<=k; ktmp++){
+        //Sommet de référence
         snprintf(buf, 2, "%d", i);
         write(filedesc,"-",1);
         write(filedesc,buf,1);
+        //Hauteur présumé du sommet
         snprintf(buf, 2, "%d", ktmp);
         write(filedesc,buf,1);
         write(filedesc," ",1);
+        //Parcours des voisins
         for(int j = 1; j<=n; j++){
+        // -1 car on commence la numérotation des sommets à 1, glucose ne gérant pas 0 comme variable.
           if(are_adjacent(i-1,j-1)){
+            //Sommet voisin
             snprintf(buf, 2, "%d", j);
             write(filedesc,buf,1);
+            //Hauteur - 1 (le sommet est plus haut dans le graphe)
             snprintf(buf, 2, "%d", ktmp-1);
             write(filedesc,buf,1);
             write(filedesc," ",1);
           }
         }
+        //Fin de ligne.
         write(filedesc,"0",1);
         write(filedesc,"\n",1);
         }
       }
     }
 
+/* Il exite au moins un sommet de hauteur k */
 void clause3(int filedesc, int n, int k){
-	char buf[2];
+    char buf[2];
+    //Parcours de chaque sommet
 	for(int i = 1; i<=n; i++){
+        //Sommet de référence
 		snprintf(buf, 2, "%d", i);
         write(filedesc,buf,1);
+        //Hauteur k
         snprintf(buf, 2, "%d", k);
         write(filedesc,buf,1);
         write(filedesc," ",1);
-	}
+    }
+    //Fin de ligne
 	write(filedesc,"0",1);
 	write(filedesc,"\n",1);
 }
 
 
-int main(){
-    int filedesc = open("form.cnf", O_WRONLY | O_CREAT,0666);
+int main(int argc, char *argv[]){
+    //////// ARBRE ATTENDU DE TAILLE 3 ////////////
+    int filedesc = open("form.cnf", O_WRONLY | O_CREAT | O_TRUNC,0666);
 
-    int n = 3;
-    int k = 2;
-    clause2(filedesc, n);
-    clause3(filedesc, n, k);
-    clause4(filedesc, n, k);
-
-
+    int nb_sommets = orderG();
+    //Hauteur désirée de l'arbre couvrant
+    int k = atoi(argv[1]);
+    printf("JE SUIS K :%d\n", k);
+    clause1(filedesc, nb_sommets, k);
+    clause2(filedesc, nb_sommets);
+    clause3(filedesc, nb_sommets, k);
+    clause4(filedesc, nb_sommets, k);
 }
